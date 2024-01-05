363c019181b41c28bf13399b1e34ff096e275195
tuanbv <tuanbv@saisystem.vn>
2024-01-05 17:00:21 +0700

    Add project Vue3

---
+++ Vue3/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/service',
+      name: 'service',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ServiceView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/library',
+      name: 'library',
+      component: () => import('../views/LibraryView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ Vue3/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref, watchEffect } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+const count = ref(0);
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(count.value);
+})
+
+watchEffect(() => {
+  console.log(count.value);
+})
+
+</script>
+
+<template>
+  <main>
+    <button @click="count++" class="text-orange-800">Count</button>
+
+    {{ count }}
+    <TheWelcome />
+  </main>
+</template>
