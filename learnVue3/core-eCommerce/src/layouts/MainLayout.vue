d1b28663948f01486963275e0bf36c3f3f95e0e0
TuanBV <tuanbv@saisystem.vn>
2024-07-24 10:02:31 +0700

    update

---
+++ learnVue3/core-eCommerce/src/layouts/MainLayout.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Navbar from 'components/NavBar.vue'
+</script>
+<template>
+  <Navbar />
+  <router-view></router-view>
+</template>
---
+++ learnVue3/core-travel/src/layouts/TestView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ModalBasic from 'components/modal/ModalBasic.vue'
+</script>
+<template>
+  <!-- {{ this.$globalImg.imgNo }} -->
+  <ModalBasic />
+</template>
