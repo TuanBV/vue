e545e6248670e173d8855784e585638e063c2105
tuanbv <bui.tuan.10051998@gmail.com>
2024-08-20 21:39:47 +0700

    update source

--- demo/src/components/SubNav.vue
+++ demo/src/components/SubNav.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-full h-16 border-b border-brand-gray-1 bg-white">
     <div class="flex items-center h-full px-8">
-      <i class="fa-solid fa-magnifying-glass"></i> This is sub nav
+      <font-awesome-icon :icon="['fas', 'search']" />
+      This is sub nav
     </div>
   </div>
 </template>
--- demo/src/main.js
+++ demo/src/main.js
@@ -2,12 +2,15 @@ import 'assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+library.add(faSearch)
 
 const app = createApp(App)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
