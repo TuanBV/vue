4d86fac40bb15e1f24fd11b153e5f86b8649ae9f
tuanbv <bui.tuan.10051998@gmail.com>
2024-09-04 23:46:40 +0700

    update code CV

--- CV/src/layouts/MainLayout.vue
+++ CV/src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
 import NavBar from 'components/navbar/NavBar.vue'
 </script>
 <template>
-  <div class="bg-[url('assets/images/home-background.jpg')] h-[100vh]">
+  <div class="bg-[url('assets/images/home-background.jpg')] h-[100vh] bg-cover">
     <nav-bar></nav-bar>
     <div class="pt-[84px]">
       <router-view></router-view>
--- CV/src/views/HomeView.vue
+++ CV/src/views/HomeView.vue
@@ -1,7 +1,34 @@
 <template>
   <!-- <font-awesome-icon :icon="['fas', 'search']" class="mr-3"/> -->
   <main>
-    <div class="w-full text-white">
+    <div class="w-full">
+      <section>
+        <div class="container mx-auto my-40 text-center text-white">
+        <div class="text-lg font-medium uppercase tracking-[.2rem]">
+          Don't watch the clock. Do what it does
+        </div>
+        <div class="text-[100px] font-semibold tracking-[0.5rem]">KEEP GOING</div>
+        <button
+          class="mt-10 bg-white px-5 py-3 font-bold text-black hover:bg-[#b0fcfdfd] hover:text-[#703f27]"
+        >
+          Start Now
+        </button>
+      </div>
+      </section>
+      <section class="bg-white py-40">
+        <div class="container mx-auto">
+          <h1 class="text-[56px] font-semibold">About Me.</h1>
+          <div class="leading-md">
+            This is your About section. Every website has a story and users want to hear yours. This
+            is a great opportunity to give a full background on who you are and what your site has
+            to offer. Double click on the text box to edit the content and add all the information
+            you want to share. You may like to talk about how you got started and share your
+            professional journey. Explain your core values, your commitment to customers and how you
+            stand out from the crowd. You can also add a photo, gallery or video for even more
+            engagement.
+          </div>
+        </div>
+      </section>
     </div>
   </main>
 </template>
